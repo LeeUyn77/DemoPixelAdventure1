7e90061db3ef4e879b02d8eea54c4b214cc2b0c5
LeeUyn77 <locle1907@gmail.com>
2023-07-01 09:06:36 +0700

    update

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float speed =7f;
+    float speed = 7f;
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
     private void Start()
     {
-        rb=GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        rb.velocity=new Vector2(Input.GetAxisRaw("Horizontal")*speed, rb.velocity.y);
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y);
 
-        if(Input.GetButtonDown("Jump"))
-            rb.velocity = new Vector2(rb.velocity.x,14f);
-        
+        if (Input.GetButtonDown("Jump"))
+            rb.velocity = new Vector2(rb.velocity.x, 14f);
     }
-    
 }
