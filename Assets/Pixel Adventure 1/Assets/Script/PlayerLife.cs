9489b445a7a427af11619b0ad147a03bbbb5fbdb
LeeUyn77 <locle1907@gmail.com>
2023-07-02 05:03:36 +0700

    death logic

---
+++ Assets/Pixel Adventure 1/Assets/Script/PlayerLife.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani=GetComponent<Animator>();
+        rb=GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.CompareTag("Trap")){
+            Die();
+        }
+    }
+
+    private void Die(){
+        rb.bodyType = RigidbodyType2D.Static;
+        ani.SetTrigger("death");
+    }
+
+    void RestartLevel(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
